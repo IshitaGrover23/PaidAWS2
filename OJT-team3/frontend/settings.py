3a04e381ca66e6573a89388458abc6f64801a701
IshitaGrover23 <122429946+IshitaGrover23@users.noreply.github.com>
2024-09-26 13:15:35 +0530

    Update settings.py

--- OJT-team3/frontend/settings.py
+++ OJT-team3/frontend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6%0#owkr)iy_u^yd%hhu1gj84%s!j*!qks%my@rupj^i60wyz6"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "your-default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
